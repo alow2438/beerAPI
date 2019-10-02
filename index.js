b1d63140d7416c53b253856b99284b40ef72184a
Towell <AST1128@daugherty.com>
2019-10-02 10:36:43 -0500

    Added delete functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express() //app is the tool use to 
 const beerRouter = require("./routes/beerRouter")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/beers", beerRouter)
 
@@ -10,8 +13,18 @@ app.use("/", (req, res) => { //configure middleware to run
     res.send(`<h1>Welcome to the beer API</h1>`)
 })
 
-const port = process.env.PORT || 4444  
+mongoose.connect("mongodb://localhost:27017/beers", {
+    useNewUrlParser: true
+})
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to the beers database!`)
+})
 
+mongoose.connection.on("error", (err) => {
+    console.log(`Error connecting to beers databse:\n${err}`)
+})
+
+const port = process.env.PORT || 4444  
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`) //backticks do something special with the ${port}
